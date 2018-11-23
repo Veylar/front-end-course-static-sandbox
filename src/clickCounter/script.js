f0e06561736e0c2c51c345add6cafa6122fae935
Veylar <veylar@mail.ru>
2018-11-23 17:26:58 +0300

    click-counter-v1.0

--- src/clickCounter/script.js
+++ src/clickCounter/script.js
@@ -1,10 +1,16 @@
 const clickBtn = document.getElementById('click-btn')
 const clickValue = document.getElementById('click-value')
 const clickReset = document.getElementById('click-reset')
+const clickStep = document.getElementById('click-step')
+const clickMax = document.getElementById('max-value')
+const clickStepBtn = document.getElementById('click-step-btn')
+const clickMaxBtn = document.getElementById('click-max-btn')
+const clicksleftnumber = document.getElementById('clicks-left')
 
+let maxValue = 20
 let clicks = 0
 let step = 1
-const maxValue = 10
+let clicksleft = 20
 
 clickBtn.onclick = function() {
   if (clicks < maxValue) {
@@ -12,10 +18,31 @@ clickBtn.onclick = function() {
   } else {
     clicks = 'max value reached'
   }
+clicksleft = maxValue - clicks
+ 
+clicksleftnumber.innerHTML = clicksleft
 clickValue.innerHTML = clicks
+}
+
+clickStepBtn.onclick = function() {
+ step = step + 1
+ clickStep.innerHTML = step
+}
+
+clickMaxBtn.onclick = function() {
+  maxValue = maxValue + 1
+  clicksleft = maxValue - clicks
+  clicksleftnumber.innerHTML = clicksleft
+  clickMax.innerHTML = maxValue
 }
 
 clickReset.onclick = function() {
    clicks = 0
+   step = 1
+   maxValue = 20
+   clicksleft = 20
 clickValue.innerHTML = clicks
+clickStep.innerHTML = step
+clickMax.innerHTML = maxValue
+clicksleftnumber.innerHTML = clicksleft
 }
