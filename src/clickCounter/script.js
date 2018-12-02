a9265bc24b743b1ce2f7fe794ece77717c8cd7bc
Veylar <veylar@mail.ru>
2018-12-02 19:13:05 +0300

    pony 0.0

--- src/clickCounter/script.js
+++ src/clickCounter/script.js
@@ -1,55 +1,55 @@
-const clickBtn = document.getElementById('click-btn')
-const clickValue = document.getElementById('click-value')
-const clickReset = document.getElementById('click-reset')
-const clickStep = document.getElementById('click-step')
-const clickMax = document.getElementById('max-value')
-const clickStepBtn = document.getElementById('click-step-btn')
-const clickMaxBtn = document.getElementById('click-max-btn')
-const clicksleftnumber = document.getElementById('clicks-left')
+// const clickBtn = document.getElementById('click-btn')
+// const clickValue = document.getElementById('click-value')
+// const clickReset = document.getElementById('click-reset')
+// const clickStep = document.getElementById('click-step')
+// const clickMax = document.getElementById('max-value')
+// const clickStepBtn = document.getElementById('click-step-btn')
+// const clickMaxBtn = document.getElementById('click-max-btn')
+// const clicksleftnumber = document.getElementById('clicks-left')
 
-let maxValue = 20
-let clicks = 0
-let step = 1
-let clicksleft = 20
+// let maxValue = 20
+// let clicks = 0
+// let step = 1
+// let clicksleft = 20
 
-clickBtn.onclick = function() {
-  if (clicks < maxValue-step) {
-    clicks = clicks + step
-    clicksleft = maxValue - clicks
-  } else {
-    clicks = 'max value reached'
-    clicksleft = 'no clicks left :('
-  }
+// clickBtn.onclick = function() {
+//   if (clicks < maxValue-step) {
+//     clicks = clicks + step
+//     clicksleft = maxValue - clicks
+//   } else {
+//     clicks = 'max value reached'
+//     clicksleft = 'no clicks left :('
+//   }
 
-clicksleftnumber.innerHTML = clicksleft
-clickValue.innerHTML = clicks
-}
+// clicksleftnumber.innerHTML = clicksleft
+// clickValue.innerHTML = clicks
+// }
 
-clickStepBtn.onclick = function() {
- step = step + 1
- clickStep.innerHTML = step
-}
+// clickStepBtn.onclick = function() {
+//  step = step + 1
+//  clickStep.innerHTML = step
+// }
 
-clickMaxBtn.onclick = function() {
-  if (clicks != 'max value reached') {
-    maxValue = maxValue + 1
-    clicksleft = maxValue - clicks
-  } else {
-    maxValue = "you've reached Max Value! Please, leave a message or reset"
-    clicksleft = 'no clicks left :('
-  }
+// clickMaxBtn.onclick = function() {
+//   if (clicks != 'max value reached') {
+//     maxValue = maxValue + 1
+//     clicksleft = maxValue - clicks
+//   } else {
+//     maxValue = "you've reached Max Value! Please, leave a message or reset"
+//     clicksleft = 'no clicks left :('
+//   }
 
-clickMax.innerHTML = maxValue
-clicksleftnumber.innerHTML = clicksleft
-}
+// clickMax.innerHTML = maxValue
+// clicksleftnumber.innerHTML = clicksleft
+// }
 
-clickReset.onclick = function() {
-   clicks = 0
-   step = 1
-   maxValue = 20
-   clicksleft = maxValue
-clickValue.innerHTML = clicks
-clickStep.innerHTML = step
-clickMax.innerHTML = maxValue
-clicksleftnumber.innerHTML = clicksleft
-}
+// clickReset.onclick = function() {
+//    clicks = 0
+//    step = 1
+//    maxValue = 20
+//    clicksleft = maxValue
+// clickValue.innerHTML = clicks
+// clickStep.innerHTML = step
+// clickMax.innerHTML = maxValue
+// clicksleftnumber.innerHTML = clicksleft
+// }
